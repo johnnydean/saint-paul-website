5d8268dea49f4aff4505205338e15d17ee0b8afb
Joseph Dean <joe@on-site.com>
2015-11-03 23:09:19 -0800

    Remove canonization page and move to bottom of life page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/life'
-  get 'static_pages/canonization'
   get 'static_pages/about'
   get 'static_pages/blog'
   root 'static_pages#home'
